d581e471926e4e30ecbaddcbf84060e00f60f991
Charlie Kim <charliekim451@gmail.com>
2025-02-22 19:25:38 -0700

    colorfix

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -12,6 +12,12 @@ return {
 		"sainnhe/gruvbox-material",
 		priority = 1000,
 		config = function()
+			vim.opt.background = "dark"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_foreground = "material"
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_enable_italic = 1
+
 			vim.cmd("colorscheme gruvbox-material")
 		end,
 	},
