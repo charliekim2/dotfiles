0178db79cd7bb7df5ee280a47204ba1dff354ec9
Charlie Kim <charliekim451@gmail.com>
2025-02-24 12:24:03 -0700

    path completions

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,14 +7,25 @@ return {
 	},
 	config = function()
 		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
 		telescope.setup({
 			extensions = {
 				fzf = {
-					fuzzy = true, -- false will only do exact matching
+					fuzzy = true,
 					override_generic_sorter = true, -- override the generic sorter
 					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-					-- the default case_mode is "smart_case"
+					case_mode = "smart_case",
+				},
+			},
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
 				},
 			},
 		})
