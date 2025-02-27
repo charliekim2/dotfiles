cf828b22072b9a5b6a62cc67b68ef83cb1fcca0d
Charlie Kim <charliekim451@gmail.com>
2025-02-26 20:43:06 -0700

    cursor colors

--- nvim/.config/nvim/lua/config/presets.lua
+++ nvim/.config/nvim/lua/config/presets.lua
@@ -1,6 +1,11 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
-vim.opt.guicursor = "i:blinkon1"
+
+vim.cmd([[highlight iCursor guifg=white guibg=green]])
+vim.cmd([[highlight vCursor guifg=white guibg=red]])
+vim.opt.guicursor = "i:block-iCursor-blinkon1"
+vim.opt.guicursor = vim.opt.guicursor + "v:block-vCursor"
+vim.opt.guicursor = vim.opt.guicursor + "n-v-c:blinkon0"
+
 vim.opt.termguicolors = true
 vim.opt.foldenable = false
